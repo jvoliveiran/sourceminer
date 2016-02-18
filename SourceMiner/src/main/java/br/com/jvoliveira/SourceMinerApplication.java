134c4e0bb11407dc3d93fc6742f825b6a56a7ded
Joao Victor <jvoliveiran@gmail.com>
2016-02-18 10:06:10 -0300

    Commit inicial - Projeto SourceMiner

---
+++ SourceMiner/src/main/java/br/com/jvoliveira/SourceMinerApplication.java
@@ -0,0 +1,12 @@
+package br.com.jvoliveira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceMinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourceMinerApplication.class, args);
+	}
+}
