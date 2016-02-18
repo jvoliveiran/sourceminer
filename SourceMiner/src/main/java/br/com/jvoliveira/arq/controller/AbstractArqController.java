b28c37d8e161ce915c0381f58b68508fd5103a14
Joao Victor <jvoliveiran@gmail.com>
2016-02-18 14:35:25 -0300

    Definição de arquitetura para camada de visão

---
+++ SourceMiner/src/main/java/br/com/jvoliveira/arq/controller/AbstractArqController.java
@@ -0,0 +1,102 @@
+/**
+ * 
+ */
+package br.com.jvoliveira.arq.controller;
+
+import java.lang.reflect.ParameterizedType;
+
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import br.com.jvoliveira.arq.domain.ObjectDB;
+import br.com.jvoliveira.arq.service.AbstractArqService;
+
+
+/**
+ * @author Joao Victor
+ *
+ */
+public abstract class AbstractArqController<T extends ObjectDB> {
+
+	protected T obj;
+	
+	protected String title;
+	
+	protected String path;
+	
+	protected AbstractArqService<T> service;
+	
+	protected Class<T> typeClass;
+	
+	@SuppressWarnings("unchecked")
+	public AbstractArqController(){
+		this.typeClass = (Class<T>) (
+	               (ParameterizedType) getClass().getGenericSuperclass())
+	              .getActualTypeArguments()[0];
+	}
+
+	@RequestMapping("/index")
+	public String getIndex(Model model){
+		model.addAttribute("list",service.getAll());
+		return forward("index");
+	}
+	
+	@RequestMapping("/view/{id}")
+	public String view(@PathVariable Long id, Model model){
+		model.addAttribute("obj", service.getOneById(id));
+		return forward("view");
+	}
+	
+	@RequestMapping("/create")
+	public String create(Model model) throws InstantiationException, IllegalAccessException{
+		this.obj = typeClass.newInstance();
+		model.addAttribute("obj", this.obj);
+		return forward("form");
+	}
+	
+	@RequestMapping(value="/edit", method=RequestMethod.POST)
+	public String edit(@RequestParam("idObj") Long id, Model model){
+		this.obj = this.service.getOneById(id);
+		model.addAttribute("obj", this.obj);
+		return forward("form");
+	}
+	
+	@RequestMapping(value="/create", method=RequestMethod.POST)
+	public String save(@Validated T obj, Errors errors, Model model){
+		
+		if(errors.hasErrors()){
+			model.addAttribute("obj", obj);
+			return forward("form");
+		}
+		
+		service.create(obj);
+		
+		return redirect("index");
+	}
+	
+	@RequestMapping(value="/delete", method=RequestMethod.POST)
+	public String delete(@RequestParam("idObj") Long id, Model model){
+		service.remove(id);
+		
+		return redirect("index");
+	}
+	
+	private String forward(String page){
+		return path+"/"+page;
+	}
+	
+	private String redirect(String action){
+		return "redirect:/" + path + "/" + action;
+	}
+	
+	@ModelAttribute("controllerTitle")
+	public String getTitle(){
+		return this.title;
+	}
+}
