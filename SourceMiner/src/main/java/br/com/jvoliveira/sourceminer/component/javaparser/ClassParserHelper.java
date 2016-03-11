662480b3c372ba63e9603587fdfe0c85ca41632f
Joao Victor <jvoliveiran@gmail.com>
2016-03-11 14:06:16 -0300

    DESENV - Componente ClassParserHelper

--- SourceMiner/src/main/java/br/com/jvoliveira/sourceminer/component/javaparser/ClassParserHelper.java
+++ SourceMiner/src/main/java/br/com/jvoliveira/sourceminer/component/javaparser/ClassParserHelper.java
@@ -5,17 +5,35 @@ package br.com.jvoliveira.sourceminer.component.javaparser;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import br.com.jvoliveira.sourceminer.domain.ItemAsset;
 
 /**
  * @author Joao Victor
  *
  */
+@Component
 public class ClassParserHelper {
-
-	public static List<ItemAsset> getMethodsFromFileContent(String fileContent){
-		//TODO
+	
+	JavaClassParser javaParser;
+	
+	public ClassParserHelper(){
+		
+	}
+	
+	public List<ItemAsset> getDiffMethodsBetweenFileContent(String oldContent, String actualContent){
+		
+		List<ItemAsset> oldAssets = getAllMethodsInFileContent(oldContent);
+		List<ItemAsset> actualAssets = getAllMethodsInFileContent(oldContent);
+		
+		//TODO: Implementar comparação
 		return null;
+	}
+
+	private List<ItemAsset> getAllMethodsInFileContent(String fileContent){
+		javaParser = new JavaClassParser(fileContent);
+		return javaParser.parserMethods();
 	}
 	
 }
