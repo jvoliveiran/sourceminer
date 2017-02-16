5415cc872d13d7c384e4b7cb3952ebfc69c4baa2
Joao Victor <jvoliveiran@gmail.com>
2017-02-16 17:05:44 -0300

    Classes de domínio específicas Neo4j para composição do grafo.

---
+++ SourceMiner/src/main/java/br/com/jvoliveira/sourceminer/neo4j/domain/ClassNode.java
@@ -0,0 +1,97 @@
+/**
+ * 
+ */
+package br.com.jvoliveira.sourceminer.neo4j.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Relationship;
+
+/**
+ * Nós do grafo que serão representações referentes à RepositoryItem
+ * 
+ * @author João Victor
+ *
+ */
+@NodeEntity
+public class ClassNode {
+
+	@GraphId
+	private Long id;
+	
+	private Long repositoryItemId;
+	
+	private String name;
+	
+	private String fullPath;
+	
+	/*
+	 * Classes da qual a classe representada por esse nó dependem 
+	 */
+	@Relationship(type = "CALLS", direction = Relationship.OUTGOING)
+	private List<ClassNode> dependencies = new ArrayList<>();
+	
+	@Relationship(type = "CALLS", direction = Relationship.INCOMING)
+	private List<ClassNode> dependents = new ArrayList<>();
+	
+	public ClassNode(){
+		
+	}
+	
+	public ClassNode(Long repositoryItemId, String name){
+		this.repositoryItemId = repositoryItemId;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getRepositoryItemId() {
+		return repositoryItemId;
+	}
+
+	public void setRepositoryItemId(Long repositoryItemId) {
+		this.repositoryItemId = repositoryItemId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFullPath() {
+		return fullPath;
+	}
+
+	public void setFullPath(String fullPath) {
+		this.fullPath = fullPath;
+	}
+
+	public List<ClassNode> getDependencies() {
+		return dependencies;
+	}
+
+	public void setDependencies(List<ClassNode> dependencies) {
+		this.dependencies = dependencies;
+	}
+
+	public List<ClassNode> getDependents() {
+		return dependents;
+	}
+
+	public void setDependents(List<ClassNode> dependents) {
+		this.dependents = dependents;
+	}
+	
+}
---
+++ SourceMiner/src/main/java/br/com/jvoliveira/sourceminer/neo4j/domain/MethodCall.java
@@ -0,0 +1,90 @@
+/**
+ * 
+ */
+package br.com.jvoliveira.sourceminer.neo4j.domain;
+
+import org.neo4j.ogm.annotation.EndNode;
+import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.RelationshipEntity;
+import org.neo4j.ogm.annotation.StartNode;
+
+/**
+ * @author João Victor
+ *
+ */
+@RelationshipEntity(type = "CALLS")
+public class MethodCall {
+
+	@GraphId
+	private Long id;
+	
+	private Long itemAssetId;
+	
+	private String methodName;
+	
+	private String methodSignature;
+	
+	@StartNode
+	private ClassNode caller;
+	
+	@EndNode
+	private ClassNode called;
+	
+	public MethodCall(){
+		
+	}
+	
+	public MethodCall(ClassNode caller, ClassNode called){
+		this.setCaller(caller);
+		this.setCalled(called);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getMethodSignature() {
+		return methodSignature;
+	}
+
+	public void setMethodSignature(String methodSignature) {
+		this.methodSignature = methodSignature;
+	}
+
+	public String getMethodName() {
+		return methodName;
+	}
+
+	public void setMethodName(String methodName) {
+		this.methodName = methodName;
+	}
+	
+	public Long getItemAssetId() {
+		return itemAssetId;
+	}
+
+	public void setItemAssetId(Long itemAssetId) {
+		this.itemAssetId = itemAssetId;
+	}
+
+	public ClassNode getCaller() {
+		return caller;
+	}
+
+	public void setCaller(ClassNode caller) {
+		this.caller = caller;
+	}
+
+	public ClassNode getCalled() {
+		return called;
+	}
+
+	public void setCalled(ClassNode called) {
+		this.called = called;
+	}
+	
+}
